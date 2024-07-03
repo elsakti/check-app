0b9368c66bfdee9c883ac0d505bb09513ffd5898
elsakti <svkakazone@gmail.com>
2024-07-03 12:13:02 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+const listItem = [
+  { id: 1, name: "Makan", done: false },
+  { id: 2, name: "Tidur", done: true },
+];
+
+function Logo() {
+  return <span className="logo"> My Check-App </span>;
+}
+
+function Form() {
+  return (
+    <>
+      <div className="add-form">
+        <h3>Tambahkan Catatan</h3>
+      </div>
+    </>
+  );
+}
+
+function CheckList() {
+  return (
+    <>
+      <div className="list">
+        <ul>
+          {listItem.map((item) => (
+            <Item key={item.id} item={item} />
+          ))}
+        </ul>
+      </div>
+    </>
+  );
+}
+
+function Stats() {
+  return (
+    <>
+      <footer className="stats">
+        <span>Kamu punya x catatan dan baru x yang diselesaikan (x%)</span>
+      </footer>
+    </>
+  );
+}
+
+function Item({ item }) {
+  return (
+    <li key={item.id}>
+      <input type="checkbox" />
+      <span style={{ textDecoration: item.done ? "line-through" : "" }}>
+        {item.name}
+      </span>
+      <button>X</button>
+    </li>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Logo />
+      <Form />
+      <CheckList />
+      <Stats />
     </div>
   );
 }
